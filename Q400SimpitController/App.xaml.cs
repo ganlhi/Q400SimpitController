d57d7dc163ca3ba8bcc65f506f7ed91348796d03
Ganlhi <ganlhi@gmail.com>
2021-01-15 18:12:13 +0100

    Initial commit

---
+++ Q400SimpitController/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Threading;
+
+namespace Q400SimpitController
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        private void Application_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show("An unhandled exception just occurred: " + e.Exception.Message, "Exception",
+                MessageBoxButton.OK, MessageBoxImage.Warning);
+            e.Handled = true;
+        }
+    }
+}
