bf808922a28185ce45871ff8cfe8d4f3d9335068
Benjamin Laffont <alphamax@gmail.com>
2021-01-17 23:32:11 +0100

    Moving to MVVM basis

--- Q400SimpitController/MainWindow.xaml.cs
+++ Q400SimpitController/MainWindow.xaml.cs
@@ -1,39 +1,13 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Q400SimpitController.Converter.Json;
+using Q400SimpitController.ViewModel;
+using Q400SimpitController.ViewModel.Messaging;
+using System;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Net;
-using System.Net.Sockets;
-using System.Windows;
-using Newtonsoft.Json;
 
 namespace Q400SimpitController
 {
-    class IpAddressConverter : JsonConverter
-    {
-        public override bool CanConvert(Type objectType)
-        {
-            return (objectType == typeof(IPAddress));
-        }
-
-        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
-        {
-            writer.WriteValue(value.ToString());
-        }
-
-        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
-        {
-            return IPAddress.Parse((string)reader.Value);
-        }
-    }
-    
-    public class Setup
-    {
-        public IPAddress SimPcIp {get; set;}
-        public int? SimPcPort {get; set;}
-        public int? LocalPort {get; set;}
-    } 
-    
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -41,106 +15,37 @@ namespace Q400SimpitController
     {
         #region Setup data
 
-        public Setup Setup { get; private set; }
+        public SetupVM Setup { get; private set; }
+        public CommunicationVM Communication { get; private set; }
         private readonly string _setupFilePath;
         private readonly JsonSerializerSettings _jsonSerializerSettings = new JsonSerializerSettings()
         {
-            Converters = {new IpAddressConverter()}
+            Converters = { new IpAddressConverter() }
         };
 
         #endregion
-
-        #region UI stuff 
 
         public MainWindow()
         {
             Directory.CreateDirectory(Environment.SpecialFolder.LocalApplicationData.ToString());
             _setupFilePath = Environment.SpecialFolder.LocalApplicationData + "\\setup.json";
+            Communication = new CommunicationVM(_setupFilePath, _jsonSerializerSettings);
 
-            if (File.Exists(_setupFilePath)) {
+            if (File.Exists(_setupFilePath))
+            {
                 var jsonString = File.ReadAllText(_setupFilePath);
-                Setup = JsonConvert.DeserializeObject<Setup>(jsonString, _jsonSerializerSettings);
+                Setup = new SetupVM(JsonConvert.DeserializeObject<SetupMessage>(jsonString, _jsonSerializerSettings));
             }
-            
+            else
+                Setup = new SetupVM();
+
+
             InitializeComponent();
-//            this.DataContext = this;
-            
+            SetupForm.DataContext = Setup;
+
             Console.WriteLine("MainWindow!");
             Debugger.Log(0, "trace", "MainWindow");
+
         }
-
-        private void SetupFormOnSaveSetup(object sender, Tuple<IPAddress, int, int> e)
-        {
-            Debugger.Log(0, "trace", e.ToString());
-            var (simPcIp, simPcPort, localPort) = e;
-            if (simPcIp.Equals(Setup.SimPcIp) && simPcPort == Setup.SimPcPort && localPort == Setup.LocalPort) return;
-            
-            Setup = new Setup()
-            {
-                SimPcIp = simPcIp,
-                SimPcPort = simPcPort,
-                LocalPort = localPort,
-            };
-            
-            
-            File.WriteAllText(_setupFilePath, JsonConvert.SerializeObject(Setup, _jsonSerializerSettings));
-
-            CheckStartSimPcCommunication();
-        }
-
-        #endregion
-
-        #region Sim PC communications
-
-        private UdpClient _client;
-        private IPEndPoint _endPoint;
-        private IAsyncResult _curAsyncResult;
-
-        private void CheckStartSimPcCommunication()
-        {
-            StopReceivingUdpFromSimPc();
-            if (Setup.SimPcIp == null || Setup.SimPcPort == null || Setup.LocalPort == null) return;
-            StartReceivingUdpFromSimPc();
-        }
-
-        private void StartReceivingUdpFromSimPc()
-        {
-            if (Setup.SimPcIp == null || Setup.SimPcPort == null || Setup.LocalPort == null) return;
-            _client = new UdpClient(Setup.LocalPort.Value);
-            _endPoint = new IPEndPoint(Setup.SimPcIp, Setup.SimPcPort.Value);
-            _client.Connect(_endPoint);
-
-            _curAsyncResult = _client.BeginReceive(new AsyncCallback(ReceivedUdpFromSimPc), null);
-
-            // test send
-//            var msg = Utils.BuildMessage((short) 0x1016, (short) 2, singleByte: true);
-//            _client.Send(msg, msg.Length);
-        }
-
-        private void StopReceivingUdpFromSimPc()
-        {
-            _client?.Close();
-        }
-
-        private void ReceivedUdpFromSimPc(IAsyncResult ar)
-        {
-            if (_curAsyncResult != ar) return;
-
-            try
-            {
-                var buffer = _client.EndReceive(ar, ref _endPoint);
-                Debugger.Log(0, "trace", string.Join("-", buffer.ToList().Select(b => $"{b:X}")));
-
-                _client.BeginReceive(new AsyncCallback(ReceivedUdpFromSimPc), null);
-            }
-            catch (Exception ex)
-            {
-                if (ex is ObjectDisposedException || ex is SocketException) return;
-                MessageBox.Show("An exception just occurred: " + ex.Message, "Exception",
-                    MessageBoxButton.OK, MessageBoxImage.Warning);
-            }
-        }
-
-        #endregion
     }
 }
---
+++ Q400SimpitController/ViewModel/CommunicationVM.cs
@@ -0,0 +1,90 @@
+﻿using GalaSoft.MvvmLight.Messaging;
+using Newtonsoft.Json;
+using Q400SimpitController.ViewModel.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Q400SimpitController.ViewModel
+{
+    public class CommunicationVM
+    {
+        private UdpClient _client;
+        private IPEndPoint _endPoint;
+        private IAsyncResult _curAsyncResult;
+        private string _setupFilePath;
+        private JsonSerializerSettings _jsonSerializerSettings;
+
+        private SetupMessage Setup;
+
+        public CommunicationVM(string setupFilePath, JsonSerializerSettings jsonSerializerSettings)
+        {
+            _setupFilePath = setupFilePath;
+            _jsonSerializerSettings = jsonSerializerSettings;
+            Messenger.Default.Register<SetupMessage>(this, OnSetup);
+        }
+
+        #region Event part
+        private void OnSetup(SetupMessage message)
+        {
+            if (Setup != null && Setup.SimPcIp == message.SimPcIp && Setup.SimPcPort == message.SimPcPort && Setup.LocalPort == message.LocalPort) 
+                return;
+
+            Setup = message;
+
+            File.WriteAllText(_setupFilePath, JsonConvert.SerializeObject(message, _jsonSerializerSettings));
+
+            CheckStartSimPcCommunication();
+        }
+        #endregion
+
+        private void CheckStartSimPcCommunication()
+        {
+            StopReceivingUdpFromSimPc();
+            StartReceivingUdpFromSimPc();
+        }
+
+        private void StartReceivingUdpFromSimPc()
+        {
+            _client = new UdpClient(Setup.LocalPort);
+            _endPoint = new IPEndPoint(Setup.SimPcIp, Setup.SimPcPort);
+            _client.Connect(_endPoint);
+
+            _curAsyncResult = _client.BeginReceive(new AsyncCallback(ReceivedUdpFromSimPc), null);
+            //var msg = Utils.BuildMessage((short) 0x1016, (short) 2, singleByte: true);
+            //_client.Send(msg, msg.Length);
+        }
+
+        private void StopReceivingUdpFromSimPc()
+        {
+            _client?.Close();
+        }
+
+        private void ReceivedUdpFromSimPc(IAsyncResult ar)
+        {
+            if (_curAsyncResult != ar) return;
+
+            try
+            {
+                var buffer = _client.EndReceive(ar, ref _endPoint);
+                Debugger.Log(0, "trace", string.Join("-", buffer.ToList().Select(b => $"{b:X}")));
+
+                _client.BeginReceive(new AsyncCallback(ReceivedUdpFromSimPc), null);
+            }
+            catch (Exception ex)
+            {
+                if (ex is ObjectDisposedException || ex is SocketException) return;
+                MessageBox.Show("An exception just occurred: " + ex.Message, "Exception",
+                    MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+    }
+}
